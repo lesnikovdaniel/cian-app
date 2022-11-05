742600ba67ab222c6fec9621c4c13354447b9d59
lesnikovdaniel <lesnikovdaniel@gmail.com>
2022-11-05 20:38:57 +0300

    Import

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request,send_from_directory
-import os 
+import os,openpyxl
+path = os.getcwd()+'/uploads/input.xlsx'
 
 app = Flask(__name__)
 
@@ -12,9 +13,35 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+
+def getObjectsFromImport(path):
+    table = openpyxl.load_workbook(path)
+    ws = table.active
+
+    obj_list = []
+    l = [row for row in ws.iter_rows(min_row=2,max_col=11, values_only=True)]
+    for row in l:
+        obj = {
+            'location':row[0],
+            'rooms_count':row[1],
+            'segment': row[2],
+            'floors': row[3],
+            'walls': row[4],
+            'floor_number': row[5],
+            'square': row[6],
+            'kitchen': row[7],
+            'balcony': row[8],
+            'from_metro': row[9],
+            'wall_decoration': row[10]
+        }
+        obj_list.append(obj)
+    return obj_list
+
+
 @app.route('/')
 def index():
-    return render_template('main/index.html')
+    objects = getObjectsFromImport(path)
+    return render_template('main/index.html',objects=objects)
 
 @app.route('/signin')
 def signin():
--- xlsx.py
+++ xlsx.py
@@ -1,3 +1,26 @@
-import pandas as pd
-import os 
-dataframe = pd.read_excel(os.getcwd()+'/uploads/input.xlsx')
+import os,openpyxl
+path = os.getcwd()+'/uploads/input.xlsx'
+
+def import_data(path):
+    table = openpyxl.load_workbook(path)
+    sheet = table.active
+
+    obj_list = []
+
+    for row in sheet.iter_rows():
+        obj = {
+            'location':row[0].value,
+            'rooms_count':row[1].value,
+            'segment': row[2].value,
+            'floors': row[3].value,
+            'walls': row[4].value,
+            'floor_number': row[5].value,
+            'square': row[6].value,
+            'kitchen': row[7].value,
+            'balcony': row[8].value,
+            'from_metro': row[9].value,
+            'wall_decoration': row[10].value
+        }
+        obj_list.append(obj)
+
+        return obj_list
