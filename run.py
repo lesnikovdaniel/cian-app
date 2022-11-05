511d3b38920474c42cb2f0e64eec17ca5977bdd8
lesnikovdaniel <lesnikovdaniel@gmail.com>
2022-11-05 20:55:51 +0300

    Added redirect after import

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request,send_from_directory
+from flask import Flask, render_template, request,send_from_directory, redirect
 import os,openpyxl
-path = os.getcwd()+'/uploads/input.xlsx'
+path = ''
 
 app = Flask(__name__)
 
@@ -17,29 +17,34 @@ def allowed_file(filename):
 def getObjectsFromImport(path):
     table = openpyxl.load_workbook(path)
     ws = table.active
-
     obj_list = []
-    l = [row for row in ws.iter_rows(min_row=2,max_col=11, values_only=True)]
-    for row in l:
-        obj = {
-            'location':row[0],
-            'rooms_count':row[1],
-            'segment': row[2],
-            'floors': row[3],
-            'walls': row[4],
-            'floor_number': row[5],
-            'square': row[6],
-            'kitchen': row[7],
-            'balcony': row[8],
-            'from_metro': row[9],
-            'wall_decoration': row[10]
-        }
-        obj_list.append(obj)
-    return obj_list
-
+    if os.path.isfile(path):
+        l = [row for row in ws.iter_rows(min_row=2,max_col=11, values_only=True)]
+        for row in l:
+            obj = {
+                'location':row[0],
+                'rooms_count':row[1],
+                'segment': row[2],
+                'floors': row[3],
+                'walls': row[4],
+                'floor_number': row[5],
+                'square': row[6],
+                'kitchen': row[7],
+                'balcony': row[8],
+                'from_metro': row[9],
+                'wall_decoration': row[10]
+            }
+            obj_list.append(obj)
+        return obj_list
 
 @app.route('/')
 def index():
+    path = ''
+    if os.path.isfile(os.getcwd()+'/uploads/input.xlsx'):
+        path = os.getcwd()+'/uploads/input.xlsx'
+    else:
+        path = os.getcwd()+'/uploads/empty.xlsx'
+    
     objects = getObjectsFromImport(path)
     return render_template('main/index.html',objects=objects)
 
@@ -60,7 +65,7 @@ def importFile():
     if request.method == 'POST':
         f = request.files['file']
         result = f.save(os.getcwd()+'/uploads/'+f.filename)
-        return "File saved successfully"
+        return redirect('http://localhost:5000/')
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
